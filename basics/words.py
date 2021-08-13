104c0814113a9746bd662a82d6242932d03f9616
willsmith <will.smith@fieldagent.net>
2021-08-13 14:59:13 -0500

    it's actually standard practice to have two blank lines between top level functions in python

--- basics/words.py
+++ basics/words.py
@@ -1,6 +1,7 @@
 import sys
 from urllib.request import urlopen
 
+
 def fetch_words(url):
     story = urlopen(url)
     story_words = []
@@ -14,13 +15,16 @@ def fetch_words(url):
     story.close()
     return story_words
 
+
 def print_items(items):
     for item in items:
         print(item)
 
+
 def main(url):
     words = fetch_words(url)
     print_items(words)
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
